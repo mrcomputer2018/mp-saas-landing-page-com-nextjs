21005f116e642520e731e702030698c0d8eb84fb
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-28 17:31:52 -0300

    Add homepage layout and SVG icon for the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,111 @@
 export default function Home() {
-    return <div className="">Ola mundo</div>;
+    return (
+        <main>
+            {/* primeira seção */}
+            <section>
+                <nav>
+                    <img src="" alt="" />
+
+                    <div>
+                        <button>Funcionamento</button>
+                        <button>Preços</button>
+                        <button>Login</button>
+                    </div>
+                </nav>
+
+                <h1>Simplifique Seus Estudos</h1>
+
+                <p>
+                    Deixe que nós fazemos a curadoria para você. Assine nossa plataforma e receba 
+                    todos os meses um ebook novo de programação.
+                </p>
+
+                <form>
+                    <input type="text" placeholder="Coloque seu e-mail..."/>
+                    <button>Assine Agora</button>
+                </form>
+
+                <p>
+                    Comece sua assinatura agora mesmo. Cancele quando quiser. 
+                </p>
+
+            </section>
+
+            {/* segubda seção */}
+            <section>
+                <h2>Como funciona?</h2>
+
+                <div>
+                    <img src="" alt="" />
+
+                    <ul>
+                        <li>
+                            Acesso a 1 ebook por mês
+                        </li>
+                        <li>
+                            Curadoria especial
+                        </li>
+                        <li>
+                            Cancele quando quiser
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            {/* terceira seção */}
+            <section>
+                <h2>Preço Simples e Transparente</h2>
+                <p>
+                    Pra que inúmeros planos quando nós sabemos exatamente o que é melhor para você? 
+                    Assine o nosso plano mensal Pro Premium VIP e garanta mensalmente um ebook novo de programação. 
+                    E por menos de um café por dia. 
+                </p>
+
+                <div>
+                    <h3>Plano Pro Premium VIP</h3>
+                    <p>
+                        Tudo que você precisa para seus estudos
+                    </p>
+
+                    <p>R$29/mes</p>
+
+                    <ul>
+                        <li>
+                            1 ebook por mês
+                        </li>
+
+                        <li>
+                            Curadoria especial
+                        </li>
+
+                        <li>
+                            Acesso ilimitado
+                        </li>
+
+                        <li>
+                            Cancele a qualquer momento
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            {/* quarta seção */}
+            <section>
+                <h3>Pronto para mudar sua Vida?</h3>
+                <p>
+                    Faça como milhares de outras pessoas. Assine nosso produto e tenha garantido seus estudos 
+                </p>
+
+                <button>
+                    Assine Agora
+                </button>
+                <p>
+                    Comece sua assinatura agora mesmo. Cancele quando quiser. 
+                </p>
+
+                <img src="" alt="" />
+                <p>© 2024 LivroSaaS. Todos os direitos reservados.</p>
+            </section>
+        </main>
+    );
 }
